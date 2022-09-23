d007c8ef27b01af36a704bafbb33e1efc228dc55
Javier <duranjavier@live.com>
2022-09-23 09:37:36 -0700

    Add BlackJackApp (controller), make Dealer public

---
+++ src/com/games/blackjack/controller/BlackJackApp.java
@@ -0,0 +1,10 @@
+package com.games.blackjack.controller;
+
+import com.games.blackjack.Dealer;
+
+import java.util.Scanner;
+
+class BlackJackApp {
+    private Dealer dealer;
+    private Scanner scanner = new Scanner(System.in);
+}
